1f7420ba106cc349f1a12726e272c3337d37621a
Hanho-Lee <dlgksgh2365@gmail.com>
2026-01-17 16:12:26 +0900

    first commit

---
+++ src/main/java/com/example/pms_toy/PmsToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pms_toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsToyApplication.class, args);
+	}
+
+}
